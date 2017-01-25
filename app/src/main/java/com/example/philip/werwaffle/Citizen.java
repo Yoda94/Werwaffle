bf53bc548c5c1bf4c955d987a677eada6e36951b
Prudentia350 <prudentia350@googlemail.com>
2017-01-25 12:37:49 +0100

    Added Citizen and Werewolf Classes

---
+++ app/src/main/java/com/example/philip/werwaffle/Citizen.java
@@ -0,0 +1,19 @@
+package com.example.philip.werwaffle;
+
+import android.graphics.drawable.BitmapDrawable;
+
+/**
+ * Created by Jonas on 2501.
+ */
+
+public class Citizen {
+    private BitmapDrawable rolePicture;
+    private boolean voteRight = true;
+    private boolean alive = true;
+
+    public void voteExecution(){
+        if(voteRight) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/philip/werwaffle/Werewolf.java
@@ -0,0 +1,11 @@
+package com.example.philip.werwaffle;
+
+/**
+ * Created by Jonas on 2501.
+ */
+
+public class Werewolf extends Citizen {
+    public void voteFeed(){
+
+    }
+}
