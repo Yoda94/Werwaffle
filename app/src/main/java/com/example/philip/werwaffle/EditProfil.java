f65a5ee5b35ac797ca7573b489a90a9bda67f088
Yoda94 <philip20794@gmail.com>
2017-01-25 13:57:24 +0100

    add abilety to save a user name

--- app/src/main/java/com/example/philip/werwaffle/EditProfil.java
+++ app/src/main/java/com/example/philip/werwaffle/EditProfil.java
@@ -1,10 +1,49 @@
 package com.example.philip.werwaffle;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditProfil extends AppCompatActivity {
+
+
+    public EditText inputTxt;
+    public Button saveBut;
+    public TextView outPutTxt;
+    private SharedPreferences prefSettings;
+    private SharedPreferences.Editor prefEditor;
+    private static final int PREFERENCE_MODE_PRIVATE = 0;
+
+
+    public void init(){
+        inputTxt = (EditText) findViewById(R.id.editName);
+        saveBut = (Button) findViewById(R.id.saveBut);
+        outPutTxt = (TextView) findViewById(R.id.textViewSave);
+
+        prefSettings = getPreferences(PREFERENCE_MODE_PRIVATE);
+        prefEditor = prefSettings.edit();
+        String aName = prefSettings.getString("key1", "Empty");
+
+        outPutTxt.setText(aName);
+
+        saveBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = inputTxt.getText().toString();
+                prefEditor.putString("key1", name);
+                prefEditor.commit();
+
+                String aName = prefSettings.getString("name", "No Name");
+
+                outPutTxt.setText(aName);
+            }
+        });
+    };
+
 
 
 
@@ -12,5 +51,6 @@ public class EditProfil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profil);
+        init();
     }
 }
