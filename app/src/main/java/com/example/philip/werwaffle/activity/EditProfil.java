e8e848715998303b271020c62a472ff7f648d918
Yoda94 <philip20794@gmail.com>
2017-01-26 12:17:53 +0100

    add profile, picture select and save Version 2

--- app/src/main/java/com/example/philip/werwaffle/activity/EditProfil.java
+++ app/src/main/java/com/example/philip/werwaffle/activity/EditProfil.java
@@ -1,21 +1,33 @@
 package com.example.philip.werwaffle.activity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.philip.werwaffle.R;
+import com.google.android.gms.appindexing.AppIndex;
+
+import static android.R.attr.data;
 
 public class EditProfil extends AppCompatActivity {
 
-
+    private static int RESULT_LOAD_IMG = 1;
+    String imgDecodableString;
     public EditText inputTxt;
-    public Button saveBut;
-    public TextView outPutTxt;
+    public ImageButton selectBut;
     private SharedPreferences prefSettings;
     private SharedPreferences.Editor prefEditor;
     private static final int PREFERENCE_MODE_PRIVATE = 0;
@@ -23,31 +35,82 @@ public class EditProfil extends AppCompatActivity {
 
     public void init(){
         inputTxt = (EditText) findViewById(R.id.editName);
-        saveBut = (Button) findViewById(R.id.saveBut);
-        outPutTxt = (TextView) findViewById(R.id.textViewSave);
+        selectBut = (ImageButton) findViewById(R.id.imageBut);
 
         prefSettings = getPreferences(PREFERENCE_MODE_PRIVATE);
-        //prefEditor = prefSettings.edit();
+        prefEditor = prefSettings.edit();
         String aName = prefSettings.getString("key1", "Empty");
 
-        outPutTxt.setText(aName);
+        inputTxt.setText(aName);
 
-        saveBut.setOnClickListener(new View.OnClickListener() {
+        ImageView imgView = (ImageView) findViewById(R.id.imageProfile);
+        prefSettings = getPreferences(PREFERENCE_MODE_PRIVATE);
+        prefEditor = prefSettings.edit();
+        String aimgDecodableString = prefSettings.getString("img", "None");
+        //imgView.setImageBitmap(BitmapFactory
+        //        .decodeFile(aimgDecodableString));
+        imgView.setImageBitmap(com.example.philip.werwaffle.activity.RoundedImageView.getCroppedBitmap(
+                BitmapFactory.decodeFile(aimgDecodableString),2000 ));
+
+
+
+
+        selectBut.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                String name = inputTxt.getText().toString();
-                prefEditor.putString("key1", name);
+            public void  onClick(View v){
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
+
+            }
+
+        });
+
+    };
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            // When an Image is picked
+            if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
+                    && null != data) {
+                // Get the Image from data
+
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                // Get the cursor
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn, null, null, null);
+
+                // Move to first row
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                imgDecodableString = cursor.getString(columnIndex);
+                cursor.close();
+                ImageView imgView = (ImageView) findViewById(R.id.imageProfile);
+
+                prefEditor.putString("img", imgDecodableString);
                 prefEditor.commit();
 
-                String aName = prefSettings.getString("name", "No Name");
+                // Set the Image in ImageView after decoding the String
+                //imgView.setImageBitmap(BitmapFactory
+                //        .decodeFile(imgDecodableString));#
+                imgView.setImageBitmap(com.example.philip.werwaffle.activity.RoundedImageView.getCroppedBitmap(
+                        BitmapFactory.decodeFile(imgDecodableString),2000 ));
 
-                outPutTxt.setText(aName);
+            } else {
+                Toast.makeText(this, "You haven't picked Image",
+                        Toast.LENGTH_LONG).show();
             }
-        });
+        } catch (Exception e) {
+            Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG)
+                    .show();
+        }
+
     }
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,4 +118,19 @@ public class EditProfil extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profil);
         init();
     }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        String name = inputTxt.getText().toString();
+        prefEditor.putString("key1", name);
+        prefEditor.commit();
+
+        String aName = prefSettings.getString("key1", "No Name");
+        inputTxt.setText(aName);
+    }
+
 }
