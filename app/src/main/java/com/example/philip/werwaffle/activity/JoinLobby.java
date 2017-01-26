7af821bb83113dac4c210f3d839c4f22c3ca5bdd
Yoda94 <philip20794@gmail.com>
2017-01-26 18:40:03 +0100

    netcode packed

--- app/src/main/java/com/example/philip/werwaffle/activity/JoinLobby.java
+++ app/src/main/java/com/example/philip/werwaffle/activity/JoinLobby.java
@@ -1,22 +1,82 @@
 package com.example.philip.werwaffle.activity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.philip.werwaffle.R;
+import com.example.philip.werwaffle.netcode.WiFiDirectBroadcastReceiver;
+
+import static android.R.attr.action;
+import static android.os.Looper.getMainLooper;
 
 public class JoinLobby extends AppCompatActivity {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private JoinLobby mActivity;
+    private BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
+    WifiP2pManager.PeerListListener myPeerListListener;
+
+
+    /* register the broadcast receiver with the intent values to be matched */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+
+    }
+    /* unregister the broadcast receiver */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
+
+
+    public void init2(){
+        Button scan = (Button) findViewById(R.id.button);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_lobby);
+        //Step 3. https://developer.android.com/guide/topics/connectivity/wifip2p.html
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(), null);
+        mReceiver = new WiFiDirectBroadcastReceiver(mManager, mChannel, this);
+        //Step 4.
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+
+        init2();
+
         //Enable Wifi
         WifiManager wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
         wifiManager.setWifiEnabled(true);
